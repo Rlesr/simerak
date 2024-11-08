ef64fda4f1d07181813892c33e998b6a4df49701
Ryiias <rendraramadan06@gmail.com>
2024-11-08 14:10:29 +0700

    Footer Landing page

--- src/views/landing-page.vue
+++ src/views/landing-page.vue
@@ -1,20 +1,19 @@
 <template>
-    <div class="bg-landing min-h-screen flex flex-col items-center justify-center relative">
-      <!-- Navbar - positioned absolutely with a transparent background -->
+    <div
+      class="bg-local bg-cover bg-center bg-gray-200 min-h-screen flex flex-col items-center justify-center relative pt-20"
+      style="background-image: url('/src/assets/brand/image4.png');"
+    >
       <NavBar class="absolute top-0 left-0 w-full bg-transparent flex justify-start p-10" />
 
-      <div class="w-full max-w-5md flex flex-col items-start mb-[175px] px-6"> <!-- Adjust container width and add padding -->
-        <!-- Logo on the left side with left margin -->
-        <div class="mb-4 ml-6"> <!-- Adjust margin to keep it from touching the page edge -->
-          <img src="../assets/brand/logo-simerak.png" alt="Logo Simerak" class="w-[400px] h-[137px]"> <!-- Smaller width for responsive design -->
+      <div class="w-full max-w-2lg flex flex-col items-start mb-[175px] ml-[200px] mt-[130px]">
+        <div class="mb-4 ml-6">
+          <img src="/src/assets/brand/logo-simerak.png" alt="Logo Simerak" class="w-[400px] h-[137px]" />
         </div>
-        <!-- Welcome Text -->
-        <div class="text-white mt-4 ml-6"> <!-- Align text with the logo by adding the same margin -->
+        <div class="text-white mt-4 ml-6">
           <h1 class="text-5xl font-bold mb-2">Selamat Datang di</h1>
           <h1 class="text-4xl font-bold mb-2">Sistem Informasi Manajemen Rekening Bank</h1>
           <p class="text-xl">Daerah Khusus Ibu Kota Jakarta</p>
         </div>
-        <!-- Login Button -->
         <div class="mt-9 ml-6">
           <button
             class="bg-[#F57900] text-white text-[20px] w-[150px] h-[60px] px-[10px] py-[14px] rounded-[10px] hover:bg-blue-600 transition duration-300"
@@ -25,6 +24,8 @@
           </button>
         </div>
       </div>
+
+      <Footer class="w-full" />
     </div>
   </template>
 
@@ -35,6 +36,7 @@
   export default {
     components: {
       NavBar,
+      Footer,
     },
     data() {
       return {
@@ -48,14 +50,3 @@
     },
   };
   </script>
-
-  <style scoped>
-  /* Styling background landing page */
-  .bg-landing {
-    background-image: url("../assets/brand/image4.png");
-    background-color: #e4e7ea;
-    background-size: cover;
-    background-repeat: repeat;
-    background-position: center;
-  }
-  </style>
