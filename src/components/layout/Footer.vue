19f7e91619588483d7e0c92d2758b0d24ae74845
Ryiias <rendraramadan06@gmail.com>
2024-11-11 13:10:43 +0700

    Footer Fix

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -2,10 +2,6 @@
     <footer class="relative z-1">
       <div class="bg-gradient-to-b from-transparent via-[#054083]/70 to-[#054083]/100 py-10">
         <div class="container mx-auto flex justify-between px-6 text-white">
-<<<<<<< HEAD
-
-=======
->>>>>>> e96bc32854136956e7e1dab338893bbc162fb0b6
           <!-- Bagian Kiri: Informasi BPKD -->
           <div class="w-1/3 space-y-2">
             <img src="/src/assets/brand/bpkd.png" alt="Logo DKI Jakarta" class="w-[227px] h-[64px] mb-4">
@@ -16,31 +12,7 @@
             <p class="text-white/90">Instagram: @bpkdpemprovdki</p>
             <p class="text-white/90">Youtube: @bpkddkijakarta190</p>
           </div>
-<<<<<<< HEAD
 
-          <!-- Bagian Tengah: Ikon Media Sosial -->
-          <div class="flex space-x-4 items-center text-white">
-            <a href="#" class="hover:text-[#F57900]">
-              <!-- Ikon Google -->
-              <svg class="w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.6 10h-9.6v4h5.5c-.7 2-2.6 3.3-5.5 3.3-3.2 0-5.9-2.7-5.9-6s2.7-6 5.9-6c1.4 0 2.7.5 3.7 1.3l2.8-2.8c-1.8-1.6-4.1-2.5-6.5-2.5-5.5 0-10 4.5-10 10s4.5 10 10 10c5 0 9.4-3.6 9.4-10 0-.7-.1-1.3-.2-1.9z"/></svg>
-            </a>
-            <a href="#" class="hover:text-[#F57900]">
-              <!-- Ikon Facebook -->
-              <svg class="w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M22.5 0h-21c-.8 0-1.5.7-1.5 1.5v21c0 .8.7 1.5 1.5 1.5h21c.8 0 1.5-.7 1.5-1.5v-21c0-.8-.7-1.5-1.5-1.5zm-5 12h-3v10h-5v-10h-2v-4h2v-2c0-1.1.9-2 2-2h3v4h-2c-.3 0-.5.2-.5.5v1.5h3.5l-.5 4z"/></svg>
-            </a>
-            <a href="#" class="hover:text-[#F57900]">
-              <!-- Ikon Twitter -->
-              <svg class="w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M24 4.6c-.9.4-1.8.6-2.8.7 1-.6 1.7-1.6 2-2.7-.9.5-1.9.9-3 1.1-.9-.9-2.2-1.5-3.6-1.5-2.8 0-5 2.2-5 5 0 .4 0 .7.1 1-4.1-.2-7.7-2.2-10.1-5.3-.4.7-.6 1.6-.6 2.4 0 1.6.8 3 2.1 3.8-.8 0-1.6-.2-2.2-.6v.1c0 2.3 1.6 4.2 3.8 4.6-.4.1-.8.1-1.2.1-.3 0-.6 0-.8-.1.6 1.9 2.3 3.3 4.4 3.4-1.6 1.3-3.6 2-5.7 2-.4 0-.8 0-1.1-.1 2 1.3 4.4 2 6.9 2 8.3 0 12.9-6.9 12.9-12.9v-.6c.9-.6 1.7-1.5 2.3-2.4z"/></svg>
-            </a>
-            <a href="#" class="hover:text-[#F57900]">
-              <!-- Ikon YouTube -->
-              <svg class="w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19.6 3h-15.2c-1.9 0-3.4 1.5-3.4 3.4v11.3c0 1.9 1.5 3.4 3.4 3.4h15.2c1.9 0 3.4-1.5 3.4-3.4v-11.3c0-1.9-1.5-3.4-3.4-3.4zm-8.6 13.5v-8l6.5 4-6.5 4z"/></svg>
-            </a>
-          </div>
-
-=======
-  
->>>>>>> e96bc32854136956e7e1dab338893bbc162fb0b6
           <!-- Bagian Kanan: Layanan SIMERA -->
           <div class="w-1/3 text-right mt-20">
             <h3 class="text-[#F57900] font-semibold mb-3">Layanan SIMERAK</h3>
@@ -53,10 +25,7 @@
             </ul>
           </div>
         </div>
-<<<<<<< HEAD
 
-=======
-  
         <!-- Ikon Media Sosial -->
         <div class="flex space-x-5 justify-center  items-center text-white mt-6 mb-4" >
           <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" class=" hover:text-[#F57900] transition-colors duration-300 cursor-pointer" height="1.2em" viewBox="0 0 16 16">
@@ -71,10 +40,9 @@
       <svg xmlns="http://www.w3.org/2000/svg" class=" hover:text-[#F57900] transition-colors duration-300 cursor-pointer" width="1.5em" height="1.5em" viewBox="0 0 16 16">
           <path fill="currentColor" d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334q.002-.211-.006-.422A6.7 6.7 0 0 0 16 3.542a6.7 6.7 0 0 1-1.889.518a3.3 3.3 0 0 0 1.447-1.817a6.5 6.5 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.32 9.32 0 0 1-6.767-3.429a3.29 3.29 0 0 0 1.018 4.382A3.3 3.3 0 0 1 .64 6.575v.045a3.29 3.29 0 0 0 2.632 3.218a3.2 3.2 0 0 1-.865.115a3 3 0 0 1-.614-.057a3.28 3.28 0 0 0 3.067 2.277A6.6 6.6 0 0 1 .78 13.58a6 6 0 0 1-.78-.045A9.34 9.34 0 0 0 5.026 15" />
       </svg>
-  
+
         </div>
-  
->>>>>>> e96bc32854136956e7e1dab338893bbc162fb0b6
+
         <!-- Footer Bottom Text -->
         <div class="text-center  text-sm text-[#F57900] font-medium">
           Badan Pengelola Keuangan Daerah © 2024
@@ -85,26 +53,26 @@
 
   <script>
   import { defineComponent } from 'vue';
-  
+
   export default  {
     name: 'Footer',
     defineComponent: {
        name: 'BiGoogle'
     },
   };
-  
+
   </script>
 
   <style scoped>
   .container {
     max-width: 1200px;
   }
-  
+
   /* Tambahkan smooth transition untuk hover effects */
   a {
     @apply transition-all duration-300 ease-in-out;
   }
-  
+
   /* Optional: Tambahkan backdrop filter untuk efek blur */
   footer::before {
     content: '';
@@ -117,3 +85,4 @@
     z-index: -1;
   }
   </style>
+  
