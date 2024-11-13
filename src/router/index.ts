650c65cf945294172251bc506aab2f0e719a2eea
AveJonk <zoyzaidan30@gmail.com>
2024-11-13 16:15:23 +0700

    tabel skpd completed

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import landingPage from '@/views/landing-page.vue';
 import login from '@/views/auth/login.vue';
 import register from '@/views/auth/register.vue';
+import rekeningall from '@/views/buka-rekening/buka-rekeningSKPD.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.VITE_BASE_URL),
@@ -19,6 +20,11 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: register,
+    },
+    {
+      path: '/buka-rekening',
+      name: 'buka-rekening',
+      component: rekeningall,
     },
   ],
 });
